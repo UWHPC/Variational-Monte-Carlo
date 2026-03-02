59d5f5b55b5f9c9bdddf731a92e2ffb6c579dc6d
Selyss <99344963+Selyss@users.noreply.github.com>
2026-03-02 12:11:31 -0500

    config args struct

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+
 #include "particles/particles.hpp"
 
-#include <iostream>
+struct Config {
+  int N;
+  double L;
+  int warmup_steps;
+  int measure_steps;
+  double step_size;
+  uint64_t seed;
+  int block_size;
+};
 
-int main() {
-    return 0;
+int main(int argc, char* argv[]) {
+  return 0;
 }
