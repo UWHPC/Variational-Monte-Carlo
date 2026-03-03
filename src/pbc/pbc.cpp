b07282f2517f4a914a2f40028c744fac225c9b49
Selyss <99344963+Selyss@users.noreply.github.com>
2026-03-02 22:59:12 -0500

    update

--- src/pbc/pbc.cpp
+++ src/pbc/pbc.cpp
@@ -23,6 +23,9 @@ double periodicBoundaryCondition::wrap(double x) const noexcept {
 
 // wrap x, y, z indep
 void periodicBoundaryCondition::wrap3(double &x, double &y, double &z) const noexcept {
+    x = wrap(x);
+    y = wrap(y);
+    z = wrap(z);
 }
 
 // apply min image mapping to displacement component
@@ -34,10 +37,24 @@ void periodicBoundaryCondition::displacement(
     double xi, double yi, double zi,
     double xj, double yj, double zj,
     double& dx, double& dy, double& dz) const noexcept {
+    dx = xi - xj;
+    dy = yi - yj;
+    dz = zi - zj;
+
+    dx = minImage(dx);
+    dy = minImage(dy);
+    dz = minImage(dz);
 }
 
 // compute euclidean norm of min image displacement
 double periodicBoundaryCondition::distance(
     double xi, double yi, double zi,
     double xj, double yj, double zj) const noexcept {
+
+    double dx = 0.0;
+    double dy = 0.0;
+    double dz = 0.0;
+
+    displacement(xi, yi, zi, xj, yj, zj, dx, dy, dz);
+    return std::sqrt(dx * dx + dy * dy + dz * dz);
 }
