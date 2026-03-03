6166faf2a5378c7f4de533f14139394932f10eb6
Selyss <99344963+Selyss@users.noreply.github.com>
2026-03-02 21:42:29 -0500

    impl functions

--- src/pbc/pbc.cpp
+++ src/pbc/pbc.cpp
@@ -1,11 +1,24 @@
 #include "pbc.hpp"
+#include <cmath>
 
 
 // store L and precompute invL
-periodicBoundaryCondition::periodicBoundaryCondition(double L) noexcept : L_(L), invL_(1.0 / L) {}
+periodicBoundaryCondition::periodicBoundaryCondition(double L) noexcept
+: L_{L}
+, invL_{1.0 / L}
+{ }
 
 // wrap coordinates into [0, L)
 double periodicBoundaryCondition::wrap(double x) const noexcept {
+    double k = std::floor(x * invL_); // floor of x / L. done this way to avoid dividing by L
+    x = x - k * L_;
+
+    // fix for rare float error putting x at L
+    // avoids +/- epsilon issues with floating arithmatic
+    if (x >= L_) x -= L_;
+    if (x < 0.0) x += L_;
+
+    return x;
 }
 
 // wrap x, y, z indep
