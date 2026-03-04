eb070956885c3c8ff915f2e306c4fc48fe65bfbe
juliansalvador727 <juliansalvador727@gmail.com>
2026-03-03 22:51:07 -0500

    rewrite jastrow

--- src/jastrow_pade/jastrow_pade.cpp
+++ src/jastrow_pade/jastrow_pade.cpp
@@ -4,32 +4,40 @@
 #include <cstddef>
 
 double JastrowPade::value(const Particles& particles, const PeriodicBoundaryCondition& pbc) const noexcept {
-    const std::size_t N{particles.numParticles()};
+    const std::size_t numParticles{particles.numParticles()};
 
-    const double* RESTRICT p_x{particles.posX()};
-    const double* RESTRICT p_y{particles.posY()};
-    const double* RESTRICT p_z{particles.posZ()};
+    const double* RESTRICT positionX{particles.posX()};
+    const double* RESTRICT positionY{particles.posY()};
+    const double* RESTRICT positionZ{particles.posZ()};
 
-    const double a_local{a()};
-    const double b_local{b()};
+    const double aParameter{a()};
+    const double bParameter{b()};
 
-    double jastrowPade{};
+    double jastrowPade{0.0};
 
-    for (std::size_t i = 0; i < N; ++i) {
-        for (std::size_t j = i + 1; j < N; ++j) {
-            double dx{}, dy{}, dz{};
+    for (std::size_t particleI = 0; particleI < numParticles; ++particleI) {
+        for (std::size_t particleJ = particleI + 1; particleJ < numParticles; ++particleJ) {
+            double displacementX{};
+            double displacementY{};
+            double displacementZ{};
 
-            pbc.displacement(p_x[i], p_y[i], p_z[i], p_x[j], p_y[j], p_z[j], dx, dy, dz);
-            const double r_sq{dx * dx + dy * dy + dz * dz};
-            const double r{std::sqrt(r_sq)};
+            pbc.displacement(positionX[particleI], positionY[particleI], positionZ[particleI], 
+                             positionX[particleJ], positionY[particleJ], positionZ[particleJ], 
+                            displacementX, displacementY, displacementZ);
+            const double distanceSquared{
+                displacementX * displacementX +
+                displacementY * displacementY +
+                displacementZ * displacementZ
+            };
+            const double distance{std::sqrt(distanceSquared)};
 
             // Mask to get around if statement
-            const bool degenerate{r < 1e-12};
-            const double mask{degenerate ? 0.0 : 1.0};
+            const bool particlesCoincident{distance < 1e-12};
+            const double mask{particlesCoincident ? 0.0 : 1.0};
 
             // u(r) = a*r / (1 + b*r)
-            const double denom{1.0 + b_local * r};
-            jastrowPade += mask * (a_local * r) / denom;
+            const double denominator{1.0 + bParameter * distance};
+            jastrowPade += mask * (aParameter * distance) / denominator;
         }
     }
     return jastrowPade;
@@ -39,32 +47,38 @@ void JastrowPade::addDerivatives(const Particles& particles, const PeriodicBound
                                  double* RESTRICT gradX, double* RESTRICT gradY, double* RESTRICT gradZ,
                                  double* RESTRICT lap) const noexcept {
     // NOTE: assumes gradX/gradY/gradZ/lap are zero-initialized by caller
-    const std::size_t N{particles.numParticles()};
+    const std::size_t numParticles{particles.numParticles()};
 
-    const double* RESTRICT p_x{particles.posX()};
-    const double* RESTRICT p_y{particles.posY()};
-    const double* RESTRICT p_z{particles.posZ()};
+    const double* RESTRICT positionX{particles.posX()};
+    const double* RESTRICT positionY{particles.posY()};
+    const double* RESTRICT positionZ{particles.posZ()};
 
     const double a_local{a()};
     const double b_local{b()};
 
-    for (std::size_t i = 0; i < N; ++i) {
-        for (std::size_t j = i + 1; j < N; ++j) {
-            double dx{}, dy{}, dz{};
+    for (std::size_t i = 0; i < numParticles; ++i) {
+        for (std::size_t j = i + 1; j < numParticles; ++j) {
+            double displacementX{};
+            double displacementY{};
+            double displacementZ{};
 
-            pbc.displacement(p_x[i], p_y[i], p_z[i], p_x[j], p_y[j], p_z[j], dx, dy, dz);
-            const double r_sq{dx * dx + dy * dy + dz * dz};
-            const double r{std::sqrt(r_sq)};
+            pbc.displacement(positionX[i], positionY[i], positionZ[i], 
+                             positionX[j], positionY[j], positionZ[j], 
+                            displacementX, displacementY, displacementZ);
+            const double distanceSquared{displacementX * displacementX + 
+                                         displacementY * displacementY + 
+                                         displacementZ * displacementZ};
+            const double distance{std::sqrt(distanceSquared)};
 
             // Mask to get around if statement:
-            const bool degenerate{r < 1e-12};
-            const double inv_r{degenerate ? 1.0 : 1 / r};
-            const double mask{degenerate ? 0.0 : 1.0};
+            const bool particlesCoincident{distance < 1e-12};
+            const double inv_r{particlesCoincident ? 1.0 : 1 / distance};
+            const double mask{particlesCoincident? 0.0 : 1.0};
 
             // u(r) = a*r / (1 + b*r)
             // u'(r) = a / (1 + b*r)^2
             // u''(r) = -2ab / (1 + b*r)^3
-            const double denom{1.0 + b_local * r};
+            const double denom{1.0 + b_local * distance};
             const double denom_sq{denom * denom};
             const double denom_cb{denom_sq * denom};
 
@@ -72,9 +86,9 @@ void JastrowPade::addDerivatives(const Particles& particles, const PeriodicBound
             const double usecond{-2.0 * a_local * b_local / denom_cb};
 
             // ∇_i u(r_ij) = u'(r) * (r_vec / r)
-            const double fx{uprime * dx * inv_r};
-            const double fy{uprime * dy * inv_r};
-            const double fz{uprime * dz * inv_r};
+            const double fx{uprime * displacementX * inv_r};
+            const double fy{uprime * displacementY * inv_r};
+            const double fz{uprime * displacementZ * inv_r};
 
             gradX[i] += mask * fx;
             gradY[i] += mask * fy;
