630ccb660fc47b03658beae0976d4f796d0d1b51
shsahiti <shsahiti@hotmail.com>
2026-03-05 15:39:05 -0500

    fixed blocking anals

--- src/blocking_analysis/blocking_analysis.cpp
+++ src/blocking_analysis/blocking_analysis.cpp
@@ -8,37 +8,42 @@ std::pair<double, double> BlockingAnalysis::mean_and_standard_error() const {
         // The document says if K < 2, report mean only or flag it [cite: 35]
         throw std::runtime_error("Not enough blocks to calculate Standard Error.");
     }
+    const std::size_t block_size{block_means_get().size()};
+    const double K{static_cast<double>(block_size_get())};
 
-    const double K{static_cast<double>(block_means_get().size())};
     double overall_mean{};
+    auto& block_means{block_means_get()};
 
     // calculating average
-    overall_mean = std::accumulate(block_means_get().begin(), block_means_get().end(), 0.0) / K;
+    const double inv_K{1.0 / K};
+    overall_mean = std::reduce(block_means.begin(), block_means.end(), 0.0) * inv_K;
 
     // 2. Calculate the variance between the blocks (Eq. 39)
     double variance_sum{};
-    for (double i = 0; i < K; i++) {
-        variance_sum += (block_means_get()[i] - overall_mean) * (block_means_get()[i] - overall_mean);
+
+    for (std::size_t i = 0; i < block_size; ++i) {
+        variance_sum += (block_means[i] - overall_mean) * (block_means[i] - overall_mean);
     }
-    double s_sq = variance_sum / (K - 1.0);
+    const double denom{1.0 / (K - 1.0)};
+    const double s_sq{variance_sum * denom};
 
     // 3. Calculate the Standard Error (Eq. 40)
-    double standard_error{std::sqrt(s_sq / K)};
+    const double standard_error{std::sqrt(s_sq * inv_K)};
 
     return {overall_mean, standard_error};
 }
 
-void BlockingAnalysis::add(double x) {
-    block_sum_ += x;
-    in_block_++;
+void BlockingAnalysis::add(double local_energy) {
+    block_sum_set() += local_energy;
+    in_block_set()++;
 
     // When the block is full, calculate its average and save it
-    if (in_block_ == block_size_) {
-        block_means_.push_back(block_sum_ / block_size_); // Eq. 37
+    if (in_block_get() == block_size_get()) {
+        block_means_get().push_back(block_sum_get() / block_size_get()); // Eq. 37
 
         // Reset for the next block
-        block_sum_ = 0;
-        in_block_ = 0;
+        block_sum_set() = 0;
+        in_block_set() = 0;
     }
 }
 
