1aca720ae90595b4146391a9d1934dd2b5aaaa0b
shsahiti <shsahiti@hotmail.com>
2026-03-05 13:44:02 -0500

    added blocking analysis

--- src/blocking_analysis/blocking_analysis.hpp
+++ src/blocking_analysis/blocking_analysis.hpp
@@ -1,14 +1,22 @@
 #pragma once
-
+#include <vector>
+#include <numeric>
 #include "../config/config.hpp"
 
 class BlockingAnalysis {
 private:
-    std::size_t blockSize_;
-    std::size_t K_;
-
+    std::size_t blockSize;
+    std::size_t K;
+    double blockSum{};
+    std::size_t inBlock{};
+    std::vector<double> blockMeans;
 public:
     explicit BlockingAnalysis(std::size_t blockSize);
+    std::pair<double, double> BlockingAnalysis::meanAndStandardError() const;
     void add(double x);
     bool ready() const noexcept;
+
+    
+
+
 };
